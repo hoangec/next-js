5a483e5c25cdeb1c1e93d4128af836bd1d533efa
hoangec <vohoangec@gmail.com>
2023-11-11 11:25:59 +0700

    add new in component

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -3,6 +3,7 @@ import classes from './MeetupDetail.module.css';
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
+      <p>Nội dung chi tiết meetup</p>
       <img
         src={props.image}
         alt={props.title}
